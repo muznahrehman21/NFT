574ab834662e93cf4ed9bd82f504436ba2849f1f
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-11 01:01:25 +0500

    Added the logo

--- nft_project/src/components/Header.jsx
+++ nft_project/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import logo from "../assets/logo/logo.png";
 export default function Header() {
   return (
     <div className="flex justify-between items-center w-auto max-w-[1360px] h-[100px]">
-      <div className="logo">{/* logo yaha dalo */}Logo</div>
-      <div className="home&nfttext">
-        {/* home and nft marketplace txt with links of those pages here */}HOme
+      <div className="logo">
+        {/* logo yaha dalo */}
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="flex home&nfttext">
+        {/* home and nft marketplace txt with links of those pages here */}
+        <div className="home">Home</div>
+        <div className="nftmarketplace">AI NFT GENERATION</div>
       </div>
       <div className="buttons">
         {/* buttons ka component bano then 2 times yaha import krna */}
