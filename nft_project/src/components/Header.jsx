7e9820357dec79e4acc50c05382a49946d977ca7
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-11 17:55:51 +0500

    Made the navbar

--- nft_project/src/components/Header.jsx
+++ nft_project/src/components/Header.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import logo from "../assets/logo/logo.png";
 import Button1 from "./buttons/Button1";
 import Button2 from "./buttons/Button2";
+
 export default function Header() {
   return (
-    <div className="flex justify-between items-center w-auto max-w-[1360px] h-[100px]">
-      <div className="logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="flex home&nfttext">
-        {/* home and nft marketplace txt with links of those pages here */}
-        <div className="home">
-          <p> Home</p>
+    <div className="flex justify-center px-[300px] pt-[25px] w-full">
+      <div className="flex justify-between items-center w-[1320px] h-[100px]">
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <img src={logo} alt="logo" className="h-[100px]" />
+          <p className="font-bold text-[#FF0000]"></p>
         </div>
-        <div className="nftmarketplace">
+
+        {/* Navigation Links */}
+        <div className="flex gap-8 font-medium text-[16px] text-black">
+          <p className="border-[#FE0101] border-b-2 text-[#FE0101]">HOME</p>
           <p>AI NFT GENERATION</p>
         </div>
-      </div>
-      <div className="flex items-center gap-[10px] p-[16px_32px]">
-        <Button1 />
-        <Button2 />
+
+        {/* Buttons */}
+        <div className="flex items-center gap-[10px]">
+          <Button1 />
+          <Button2 />
+        </div>
       </div>
     </div>
   );
