1a5780f6db234d1bb0f289622eb3d06e397c368e
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-11 17:11:32 +0500

    fixed the buttons

--- nft_project/src/components/buttons/Button2.jsx
+++ nft_project/src/components/buttons/Button2.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Button2() {
   return (
-    <div className="bg-gradient-to-r from-btn1 to-btn2 p-[1.8px] rounded-lg">
-      <button className="bg-white px-8 py-3 rounded-lg font-bold text-btn1 text-sm">
+    <div className="inline-block bg-gradient-to-r from-[#FF5F5F] to-[#FF0000] p-[2px] rounded-[8px] w-[213px] h-[58px]">
+      <button className="bg-white rounded-[6px] w-full h-full font-roboto font-semibold text-[#FE0101] text-[21px] leading-none">
         Create NFT
       </button>
     </div>
