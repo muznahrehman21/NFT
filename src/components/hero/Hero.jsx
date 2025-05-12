effc7e5502eadb5a516e6121d7c6e79a43500b9b
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-12 12:21:49 +0500

    minor changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
           DISCOVER, CREATE & <br />
           SELL ARTWORKS.
         </h1>
-        <p className="mt-6 text-[#1a1a1a] text-lg">
+        <p className="font-roboto font-light text-[#808080] text-[18px] text-center align-middle leading-[100%] tracking-[0.03em]">
           Discover and trade unique digital art pieces on our NFT website,
           <br />
           where creativity meets blockchain technology.
