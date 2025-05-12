610ef56108a3645cfb0d4ba3ab98014d2906bcd1
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-12 15:26:00 +0500

    Added cursor-pointer

--- src/components/buttons/Button1.jsx
+++ src/components/buttons/Button1.jsx
@@ -9,13 +9,13 @@ function Button1({
 }) {
   return borderG ? (
     <div className="inline-block bg-gradient-to-r from-[#FF5F5F] to-[#FF0000] p-[2px] rounded-[8px] w-[213px] h-[58px]">
-      <button className="bg-white rounded-[6px] w-full h-full font-roboto font-semibold text-[#FE0101] text-[21px] leading-none">
+      <button className="bg-white rounded-[6px] w-full h-full font-roboto font-semibold text-[#FE0101] text-[21px] leading-none cursor-pointer">
         {Btn}
       </button>
     </div>
   ) : (
     <button
-      className={`${bg} px-8 py-4 border-[1.8px] rounded-[8px] h-[58px] font-roboto font-semibold text-[21px] leading-none ${textcolor} ${width}`}
+      className={`${bg} px-8 py-4 border-[1.8px] rounded-[8px] h-[58px] cursor-pointer font-roboto font-semibold text-[21px] leading-none ${textcolor} ${width}`}
     >
       {Btn}
     </button>
