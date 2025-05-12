fd8c1be084369507352f9939395ed6ca415afb99
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-12 10:36:35 +0500

    minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../assets/logo/logo.png";
 import Button1 from "./buttons/Button1";
-import Button2 from "./buttons/Button2";
+
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
@@ -9,10 +9,10 @@ export default function Header() {
     <div className="flex justify-center px-[300px] pt-[25px] w-full">
       <div className="flex justify-between items-center w-[1320px] h-[100px]">
         <div className="flex items-center gap-2">
+          {/* Logo */}
           <img src={logo} alt="logo" className="h-[100px]" />
           <p className="font-bold text-[#FF0000]"></p>
         </div>
-
         {/* Navigation Links */}
         <div className="flex gap-8 font-medium text-[16px] text-black">
           <NavLink
@@ -46,6 +46,7 @@ export default function Header() {
             width="w-[176px]"
             textcolor="text-[#FE0101] "
             bg="bg-[#FFFFFF]"
+            borderG
           />
         </div>
       </div>
--- src/components/buttons/Button1.jsx
+++ src/components/buttons/Button1.jsx
@@ -5,8 +5,15 @@ function Button1({
   width = "w-[213px]",
   textcolor = "text-[#FFFFFF]",
   bg = "bg-gradient-to-b from-[#FF5F5F] to-[#FF0000]",
+  borderG = false,
 }) {
-  return (
+  return borderG ? (
+    <div className="inline-block bg-gradient-to-r from-[#FF5F5F] to-[#FF0000] p-[2px] rounded-[8px] w-[213px] h-[58px]">
+      <button className="bg-white rounded-[6px] w-full h-full font-roboto font-semibold text-[#FE0101] text-[21px] leading-none">
+        {Btn}
+      </button>
+    </div>
+  ) : (
     <button
       className={`${bg} px-8 py-4 border-[1.8px] rounded-[8px] h-[58px] font-roboto font-semibold text-[21px] leading-none ${textcolor} ${width}`}
     >
