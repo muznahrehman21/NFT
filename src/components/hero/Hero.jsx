a9b8c6efaef7a05279f6a763ac06635b3729a91b
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-12 12:26:13 +0500

    minor changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,9 +4,9 @@ function Hero() {
   return (
     <div className="bg-[#FFF9F9] py-20 w-full">
       <div className="mx-auto px-4 max-w-[1146px] text-center">
-        <h1 className="font-apex font-bold text-[#2B2B2B] text-[64px] uppercase leading-[72px]">
-          DISCOVER, CREATE & <br />
-          SELL ARTWORKS.
+        <h1 className="font-apex font-normal text-[#2B2B2B] text-[96px] text-center capitalize leading-[102px] tracking-[0.04em]">
+          Discover, Create & <br />
+          Sell Artworks.
         </h1>
         <p className="font-roboto font-light text-[#808080] text-[18px] text-center align-middle leading-[100%] tracking-[0.03em]">
           Discover and trade unique digital art pieces on our NFT website,
