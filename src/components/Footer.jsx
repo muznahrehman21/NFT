3b3b100de1a70a6b316479ea646657c5fd5dcc9c
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-13 15:13:12 +0500

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center w-[1320px] h-[632px]">
-      <section className="text-center">
-        <h1 className="font-oxanium font-bold text-[#2B2B2B] text-[64px]">
-          READY FOR NEXT NFT DROP?
-        </h1>
-        <Input />
-      </section>
+    <div className="flex flex-col justify-center items-center space-y-[45px] w-[1320px] h-[632px]">
+      <h1 className="font-oxanium font-bold text-[#2B2B2B] text-[64px] text-center">
+        READY FOR NEXT NFT DROP?
+      </h1>
+      <Input />
     </div>
   );
 }
