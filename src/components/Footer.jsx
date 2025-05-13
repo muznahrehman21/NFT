7f4446e9e4dd61bc5d76f0e37598f094e625197d
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-13 21:51:27 +0500

    Font added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="flex flex-col justify-center items-center space-y-[45px] w-[1320px] h-[632px]">
-      <h1 className="font-oxanium font-bold text-[#2B2B2B] text-[64px] text-center">
-        READY FOR NEXT NFT DROP?
+      <h1 className="font-oxanium font-extrabold text-[#2B2B2B] text-[64px] text-center">
+        Ready for Next NFT Drop?
       </h1>
       <Input />
     </div>
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -8,12 +8,37 @@ function Input({
 }) {
   return (
     <BorderWrapper>
-      <div className="flex justify-center items-center ml-8 w-full h-full">
+      <div className="flex justify-between items-center px-4 w-full h-full">
         <input
           type={type}
           placeholder={placeholder}
-          className={`flex-1 font-roboto text-[20px] font-medium outline-none   bg-transparent ${textC} placeholder:${textC}`}
+          className={`flex-1 font-roboto text-[20px] font-medium outline-none bg-transparent ${textC} placeholder:${textC}`}
         />
+
+        {/* Red Gradient Button with White Outline Plane Icon */}
+        <button
+          type="submit"
+          className="flex justify-center items-center rounded-[15px] w-[88px] h-[76px]"
+          style={{
+            background:
+              "linear-gradient(317.62deg, #FD0000 26.88%, #FF9292 105.85%)",
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="white"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M22 2 11 13" />
+            <path d="m22 2-7 20-4-9-9-4Z" />
+          </svg>
+        </button>
       </div>
     </BorderWrapper>
   );
