974006babe0f010bd99d41b7fa9e27b8dd75fb97
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-14 14:34:24 +0500

    Added SVGs and logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,42 @@ export default function Footer() {
       </h1>
       <Input />
 
-      <div className="flex flex-row justify-center bg-gradient-to-r from-[#FF5F5F] to-[#FF0000] rounded-[20px] w-[1320px] h-[358px]">
-        <div className="flex justify-between bg-transparent">
-          <div>logo</div>
-          <div>NFT</div>
-          <div>about</div>
+      <div className="flex flex-row justify-center items-center bg-gradient-to-r from-[#FF5F5F] to-[#FF0000] rounded-[20px] w-[1320px] h-[358px]">
+        <div className="flex justify-between bg-amber-200 p-4 w-[1064px] h-[264px]">
+          <div className="flex flex-col justify-between bg-amber-500 w-[520px]">
+            <img
+              src="/Logowhite.svg"
+              alt="logo"
+              className="w-[195px] h-[130.95px]"
+            />
+          </div>
+          <div className="bg-blue-700">
+            <h2>
+              NFT ALGO is the world's leading NFTs marketplace where you can
+              discover,sell and bid NFTs and get rich.
+            </h2>
+            <div className="flex flex-row items-center gap-2.5">
+              <span>
+                <img src="/facebook.svg" alt="" />
+              </span>
+              <span>
+                <img src="/telegram.svg" alt="" />
+              </span>
+              <span>
+                <img src="/LinkdIn.svg" alt="" />
+              </span>
+              <span>
+                <img src="/Discord.svg" alt="" />
+              </span>
+              <span>
+                <img src="/insta.svg" alt="" />
+              </span>
+              <span>
+                <img src="/tiktok.svg" alt="" />
+              </span>
+            </div>
+          </div>
+          <div className="bg-fuchsia-500">About</div>
         </div>
       </div>
     </div>
