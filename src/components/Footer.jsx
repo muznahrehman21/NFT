92ede3c48c0891e64b49daa78ec7fef48ae697ba
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-14 15:48:07 +0500

    Fixed the footerr li'l bit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,20 +10,20 @@ export default function Footer() {
       <Input />
 
       <div className="flex flex-row justify-center items-center bg-gradient-to-r from-[#FF5F5F] to-[#FF0000] rounded-[20px] w-[1320px] h-[358px]">
-        <div className="flex justify-between bg-amber-200 p-4 w-[1064px] h-[264px]">
-          <div className="flex flex-col justify-between bg-amber-500 w-[520px]">
+        <div className="flex justify-between bg-gray-800 p-4 w-[1000px] h-[264px]">
+          <div className="flex flex-col justify-between bg-amber-500 w-[220px]">
             <img
               src="/Logowhite.svg"
               alt="logo"
               className="w-[195px] h-[130.95px]"
             />
           </div>
-          <div className="bg-blue-700">
-            <h2>
+          <div className="flex flex-col gap-15 bg-blue-700 p-7">
+            <div className="bg-emerald-500 max-w-77 font-roboto font-medium text-white text-base leading-relaxed">
               NFT ALGO is the world's leading NFTs marketplace where you can
               discover,sell and bid NFTs and get rich.
-            </h2>
-            <div className="flex flex-row items-center gap-2.5">
+            </div>
+            <div className="flex flex-row items-center gap-2.5 bg-amber-950">
               <span>
                 <img src="/facebook.svg" alt="" />
               </span>
@@ -42,9 +42,20 @@ export default function Footer() {
               <span>
                 <img src="/tiktok.svg" alt="" />
               </span>
+              <span>
+                <img src="/git.svg" alt="" />
+              </span>
             </div>
           </div>
-          <div className="bg-fuchsia-500">About</div>
+          <div className="bg-fuchsia-500">
+            <ul>
+              <li>About</li>
+              <li>About NFT</li>
+              <li>Live Auctions</li>
+              <li>Collection</li>
+              <li>Activity</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
