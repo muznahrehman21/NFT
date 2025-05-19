970167bb0765135b3aa00cd424be4ef02a7f3001
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-19 12:33:22 +0500

    Trending NFT'S completed

---
+++ src/components/TrendingNFT.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Button1 from "./buttons/Button1";
+import Card from "./cards/Card";
+
+function TrendingNFT() {
+  return (
+    <div className="flex flex-col gap-8 w-[1318px] h-[984px]">
+      <div className="flex justify-between items-center">
+        <h1 className="font-apex font-normal text-black text-6xl">
+          TRENDING NFT'S
+        </h1>
+        <Button1
+          width="w-[300px]"
+          height="h-[52px]"
+          Btn="Last 30 minutes"
+          textcolor="#FE0101"
+          textsize="18 px"
+        ></Button1>
+      </div>
+      <div className="flex flex-col gap-7">
+        <div className="flex flex-row gap-7">
+          <Card avatar="/C5.png" />
+          <Card avatar="/C6.png" />
+          <Card avatar="/C7.png" />
+          <Card avatar="/C8.png" />
+        </div>
+        <div className="flex flex-row gap-7">
+          <Card avatar="/C9.png" />
+          <Card avatar="/C10.png" />
+          <Card avatar="/C11.png" />
+          <Card avatar="/C12.png" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TrendingNFT;
--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -3,13 +3,15 @@ import Hero from "../../components/hero/Hero";
 
 import FeaturedCollection from "../../components/FeaturedCollection";
 import Footer from "../../components/Footer";
+import TrendingNFT from "../../components/TrendingNFT";
 
 export default function LandingPage() {
   return (
     <>
       <Hero />
-      <div className="flex flex-col items-center gap-35">
+      <div className="flex flex-col items-center gap-55">
         <FeaturedCollection />
+        <TrendingNFT />
         <Footer />
       </div>
     </>
