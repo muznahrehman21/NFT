3f6dd1186aea5e0db2c1b8ab2acea20af33dbb1d
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-21 12:25:21 +0500

    minor changes

--- src/components/buttons/Diamondbutton.jsx
+++ src/components/buttons/Diamondbutton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DiamondButton({ icon = "/r.png" }) {
   return (
-    <button className="flex justify-center items-center bg-gradient-to-b from-[#FF5F5F] to-[#FF0000] shadow-inner rounded-xl w-12 h-12 rotate-[-45deg]">
+    <button className="flex justify-center items-center bg-gradient-to-b from-[#FF5F5F] to-[#FF0000] shadow-inner rounded-xl w-12 h-12 rotate-[-45deg] cursor-pointer">
       <img src={icon} alt="" className="w-5 h-5 rotate-[45deg]" />
     </button>
   );
