f1dfa1c0bfe99bb4b04dfbd3826c67a2a3241e9c
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-23 22:48:14 +0500

    minor fixes

--- src/components/Imagination.jsx
+++ src/components/Imagination.jsx
@@ -4,7 +4,7 @@ function Imagination() {
   return (
     <div className="flex justify-center items-center py-12 w-[1320px]">
       <h1 className="max-w-5xl font-apex font-sans font-normal text-black text-6xl text-center uppercase">
-        Turn Your Imagination <br /> Into Digital Asset
+        Turn Your Imagination Into Digital Asset
       </h1>
       <div className=""></div>
     </div>
