18499993f9540c0f4fb8099b6bcee5b4915df253
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-23 23:33:17 +0500

    minor fixes

--- src/components/Imagination.jsx
+++ src/components/Imagination.jsx
@@ -7,12 +7,18 @@ function Imagination() {
         Turn Your Imagination Into Digital Asset
       </h1>
 
-      <div className="flex flex-col bg-amber-800 w-7xl">
+      <div className="bg-amber-800 w-7xl">
         <div className="flex flex-col gap-7 bg-amber-300 w-3xs">
           <img src="/im1.png" alt="" className="ml-15 w-[120px] h-[110px]" />
           <img src="/im2.png" alt="" className="ml-15 w-[170px] h-[170px]" />
           <img src="/im3.png" alt="" className="ml-1 w-[120px] h-[120px]" />
           <img src="/im4.png" alt="" className="ml-30 w-[120px] h-[120px]" />
+        </div>
+        <div className="gap-7 bg-amber-300 w-3xs">
+          <img src="/im5.png" alt="" className="ml-15 w-[120px] h-[110px]" />
+          <img src="/im6.png" alt="" className="ml-15 w-[170px] h-[170px]" />
+          <img src="/im7.png" alt="" className="ml-1 w-[120px] h-[120px]" />
+          <img src="/im8.png" alt="" className="ml-30 w-[120px] h-[120px]" />
         </div>
       </div>
     </div>
