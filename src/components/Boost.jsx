fc15332b507ff12cc32c8959d15b17dc7115720e
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-25 00:17:25 +0500

    minor fixes

--- src/components/Boost.jsx
+++ src/components/Boost.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button1 from "./buttons/Button1";
-
+import Card from "../components/cards/Card";
 function Boost() {
   return (
     <div className="relative flex w-[1320px] h-[611px]">
@@ -30,6 +30,9 @@ function Boost() {
             showIcon={true}
           />
         </div>
+      </div>
+      <div className="relative flex justify-end w-[554px]">
+        <img src="/z.png" alt=" " className="bottom-13 left-21 absolute" />
       </div>
     </div>
   );
