fbc67af41a4bafa55524d92d186e8d3b02aa0a35
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-24 11:34:16 +0500

    minor fixes

--- src/components/Imagination.jsx
+++ src/components/Imagination.jsx
@@ -7,18 +7,29 @@ function Imagination() {
         Turn Your Imagination Into Digital Asset
       </h1>
 
-      <div className="bg-amber-800 w-7xl">
-        <div className="flex flex-col gap-7 bg-amber-300 w-3xs">
-          <img src="/im1.png" alt="" className="ml-15 w-[120px] h-[110px]" />
-          <img src="/im2.png" alt="" className="ml-15 w-[170px] h-[170px]" />
-          <img src="/im3.png" alt="" className="ml-1 w-[120px] h-[120px]" />
-          <img src="/im4.png" alt="" className="ml-30 w-[120px] h-[120px]" />
+      {/* Main layout: left - center - right */}
+      <div className="flex w-full h-full">
+        {/* Left column */}
+        <div className="flex flex-col gap-7 bg-amber-300 p-4">
+          <img src="/im1.png" alt="" className="w-[120px] h-[110px]" />
+          <img src="/im2.png" alt="" className="w-[170px] h-[170px]" />
+          <img src="/im3.png" alt="" className="w-[120px] h-[120px]" />
+          <img src="/im4.png" alt="" className="w-[120px] h-[120px]" />
         </div>
-        <div className="gap-7 bg-amber-300 w-3xs">
-          <img src="/im5.png" alt="" className="ml-15 w-[120px] h-[110px]" />
-          <img src="/im6.png" alt="" className="ml-15 w-[170px] h-[170px]" />
-          <img src="/im7.png" alt="" className="ml-1 w-[120px] h-[120px]" />
-          <img src="/im8.png" alt="" className="ml-30 w-[120px] h-[120px]" />
+
+        {/* Center box (your future input area) */}
+        <div className="relative flex flex-grow justify-center items-center bg-amber-800">
+          <div className="bottom-8 absolute flex justify-center items-center bg-amber-300 w-[737px] h-[70px]">
+            hi
+          </div>
+        </div>
+
+        {/* Right column */}
+        <div className="flex flex-col gap-7 bg-amber-300 p-4">
+          <img src="/im5.png" alt="" className="w-[170px] h-[170px]" />
+          <img src="/im6.png" alt="" className="w-[110px] h-[110px]" />
+          <img src="/im7.png" alt="" className="w-[160px] h-[160px]" />
+          <img src="/im8.png" alt="" className="w-[160px] h-[160px]" />
         </div>
       </div>
     </div>
