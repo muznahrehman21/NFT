2370d73b9d46263a356ae37b2bee2064e8b56323
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-28 20:30:04 +0500

    fixes

--- src/pages/AuctionPage.jsx
+++ src/pages/AuctionPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AuctionPage() {
   return (
-    <div>
+    <div className="w-full">
       <div>
         <h1 className="bg-amber-400">AUCTION</h1>
       </div>
