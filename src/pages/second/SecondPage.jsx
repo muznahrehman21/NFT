171b37cbd63d4f7bbbabbd9b68078ed8007fbd91
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-29 10:13:19 +0500

    initiated input fields

--- src/pages/second/SecondPage.jsx
+++ src/pages/second/SecondPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Master from "../../components/Master";
 import MasterArt from "../../components/MasterArt";
+import InputMaster from "../../components/inputmaster";
 
 function SecondPage() {
   return (
     <div className="flex flex-col justify-center items-center">
       <Master />
       <MasterArt />
+      <div className="flex justify-center mt-20">
+        <InputMaster />
+      </div>
     </div>
   );
 }
