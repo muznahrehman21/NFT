c21b313c03fdb0ccbfa36509cbca8703db7760e9
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-30 20:57:21 +0500

    minor changes

--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -4,9 +4,9 @@ function Master() {
   return (
     <div className="relative flex flex-row gap-10 mt-20 font-apex">
       <h1 className="font-bold text-[#2B2B2B] text-[64px]">CREATE YOUR OWN</h1>
-      <img src="m1.svg" alt="" className="right-48 bottom-13 absolute" />
-      <img src="m2.svg" alt="" className="-right-6 bottom-0 absolute" />
-      <h1 className="font-bold text-[#FE0101] text-[64px]">MASTERPICE</h1>
+      <img src="m1.svg" alt="" className="right-56 bottom-13 absolute" />
+      <img src="m2.svg" alt="" className="-right-7 bottom-0 absolute" />
+      <h1 className="font-bold text-[#FE0101] text-[64px]">MASTERPIECE</h1>
     </div>
   );
 }
