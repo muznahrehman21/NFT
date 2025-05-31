47173744c8eb7221b6c7f12ec1d9ee669c43b1eb
muznahrehman21 <mail2m.rehman@gmail.com>
2025-05-31 10:26:56 +0500

    minor fixes

--- src/components/AddStyles.jsx
+++ src/components/AddStyles.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function AddStyles() {
-  return <div className="bg-blue-700 w-[198px] h-14">AddStyles</div>;
+  return (
+    <div className="flex justify-around items-center p-4 border-[#E7E7E7] border-[2.8px] rounded-2xl w-[198px] h-14">
+      <p className="font-roboto font-normal text-[#808080] text-[18px]">
+        {" "}
+        Add Styles
+      </p>
+      <button className="cursor-pointer">
+        <img src="/add.svg" alt="plus" className="" />
+      </button>
+    </div>
+  );
 }
 
 export default AddStyles;
