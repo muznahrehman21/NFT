50781c80fc7be249e043bd7dc4f0d37b824d4a99
muznahrehman21 <mail2m.rehman@gmail.com>
2025-06-02 23:28:39 +0500

    AuctionPage.jsx initiated

---
+++ src/components/AuctionArt.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AuctionArt() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-3 mt-8">
+      <h1 className="font-apex font-bold text-[#FE0101] text-[150px]">
+        AUCTION
+      </h1>
+
+      <div className="gap-4 grid grid-cols-4 w-[1273px h-[293px ] ]"></div>
+    </div>
+  );
+}
+
+export default AuctionArt;
--- src/pages/AuctionPage.jsx
+++ src/pages/AuctionPage.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import AuctionArt from "../components/AuctionArt";
 
 function AuctionPage() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div>
-        <h1 className="bg-amber-400">AUCTION</h1>
-      </div>
+    <div className="flex flex-col justify-center items-center gap-3">
+      <AuctionArt />
     </div>
   );
 }
