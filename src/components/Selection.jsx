c84679445acf491b6280afe3bbdd4206e4c0cb6e
muznahrehman21 <mail2m.rehman@gmail.com>
2025-06-02 16:54:05 +0500

    passed the props

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Selection() {
+function Selection({ image = "/select1.svg", text = "Cartoon" }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-1 bg-[#E7E7E7] w-[211px] h-[238px]">
-      <img src="/select1.svg " alt="" />
+    <div className="flex flex-col justify-center items-center gap-1 bg-[#E7E7E7] rounded-[15px] w-[211px] h-[238px]">
+      <img src={image} alt="" />
       <div className="flex justify-center items-center bg-white shadow-[4px_4px_15px_0_rgba(0,0,0,0.2)] rounded-[8px] w-[201px] h-[44px]">
         <p className="font-roboto font-medium text-[#2B2B2B] text-[16px]">
-          Cartoon
+          {text}
         </p>
       </div>
     </div>
--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -11,9 +11,13 @@ function Styles() {
         </h1>
         <img src="/line.svg" alt="" className="top-14 absolute" />
       </div>
-      <div className="grid grid-cols-2 mt-10">
+      <div className="gap-5 grid grid-cols-2 mt-10">
         <None />
         <Selection />
+        <Selection image="/select2.svg" text="3D Model" />
+        <Selection image="/select3.svg" text="Anime Style" />
+        <Selection image="/select4.svg" text="Fantasy art" />
+        <Selection image="/select5.svg" text="Realistic" />
       </div>
     </div>
   );
