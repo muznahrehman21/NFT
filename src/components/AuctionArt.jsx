27068f1339c25568a8cb129c6b55ab8933bc0ac0
muznahrehman21 <mail2m.rehman@gmail.com>
2025-06-03 12:15:07 +0500

    Added the pictures

--- src/components/AuctionArt.jsx
+++ src/components/AuctionArt.jsx
@@ -7,7 +7,21 @@ function AuctionArt() {
         AUCTION
       </h1>
 
-      <div className="gap-4 grid grid-cols-4 w-[1273px h-[293px ] ]"></div>
+      <div className="items-end gap-6 grid grid-cols-4 w-[1273px] h-[293px]">
+        <div className="flex justify-center items-center bg-white shadow-[4px_4px_15px_0_rgba(0,0,0,0.2)] rounded-[25px] w-[292px] h-[293px]">
+          {" "}
+          <img src="/auc1.svg" alt="" className="w-[272px] h-[273px]" />
+        </div>
+        <div className="flex justify-center items-center bg-white shadow-[4px_4px_15px_0_rgba(0,0,0,0.2)] rounded-[25px] w-[292px] h-[182px]">
+          <img src="/auc2.svg" alt="" className="w-[272px] h-[162px]" />
+        </div>
+        <div className="flex justify-center items-center bg-white shadow-[4px_4px_15px_0_rgba(0,0,0,0.2)] rounded-[25px] w-[292px] h-[182px]">
+          <img src="/auc3.svg" alt="" className="w-[272px] h-[162px]" />
+        </div>
+        <div className="flex justify-center items-center bg-white shadow-[4px_4px_15px_0_rgba(0,0,0,0.2)] rounded-[25px] w-[292px] h-[293px]">
+          <img src="/auc4.svg" alt="" className="w-[272px] h-[273px]" />
+        </div>
+      </div>
     </div>
   );
 }
