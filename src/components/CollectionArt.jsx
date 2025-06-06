821e3b3a5e693c6b3977644c13931152b95a0949
muznahrehman21 <mail2m.rehman@gmail.com>
2025-06-06 19:42:54 +0500

    TopSellersPage.jsx completed

--- src/components/CollectionArt.jsx
+++ src/components/CollectionArt.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 
-function CollectionArt() {
+function CollectionArt({
+  number = "1",
+  amount = "100",
+  img = "/collection1.svg",
+}) {
   return (
-    <div className="flex flex-row items-center gap-5 bg-[#FFF9F9] p-4 border-[#E7E7E7] border-[2.8px] rounded-[8px]">
-      <div className="flex flex-row items-center gap-4">
+    <div className="flex flex-row items-center gap-43 bg-[#FFF9F9] p-4 border-[#E7E7E7] border-[2.8px] rounded-[8px] w-full">
+      <div className="flex flex-row items-center gap-9 w-120">
         <p className="font-roboto font-semibold text-[20.87px] text-black">
-          01
+          {number}
         </p>
-        <img src="/collection1.svg" alt="" />
+        <img src={img} alt="" />
         <p className="font-roboto font-bold text-[#2B2B2B] text-[22px]">
           Jacob Jones
         </p>
       </div>
-      <div className="flex flex-row items-center gap-4">
-        <img src="/coin.png" alt="" className="bg-black rounded-full" />
-        <p className="font-roboto font-medium text-[16.24px] text-black">
-          154.00
-        </p>
-        <p className="font-roboto font-medium text-[16.24px] text-black">100</p>
-        <p className="font-roboto font-medium text-[#4CCB9E] text-[16.24px]">
-          +237.67%
-        </p>
-        <img src="/coin.png" alt="" className="bg-black rounded-full" />
-        <p className="font-roboto font-medium text-[16.24px] text-black">
-          4,901
-        </p>
-        <p className="font-roboto font-medium text-[16.24px] text-black">
-          9.8k
-        </p>
+      <div className="flex flex-row justify-between items-center p-4 w-full">
+        <div className="flex flex-row items-center gap-3">
+          <img src="/coin.png" alt="" className="bg-black rounded-full" />
+          <p className="font-roboto font-medium text-[16.24px] text-black">
+            154.00
+          </p>
+        </div>
+        <div>
+          <p className="font-roboto font-medium text-[16.24px] text-black">
+            {amount}
+          </p>
+        </div>
+        <div>
+          <p className="font-roboto font-medium text-[#4CCB9E] text-[16.24px]">
+            +237.67%
+          </p>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-3">
+          <img src="/coin.png" alt="" className="bg-black rounded-full" />
+          <p className="font-roboto font-medium text-[16.24px] text-black">
+            4,901
+          </p>
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <p className="font-roboto font-medium text-[16.24px] text-black">
+            9.8k
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/TopSellersPage.jsx
+++ src/pages/TopSellersPage.jsx
@@ -8,7 +8,23 @@ function TopSellersPage() {
     <div>
       <SellerHeader />
       <Collections />
-      <CollectionArt />
+      <div className="flex flex-col items-center gap-4 mt-8">
+        <CollectionArt />
+        <CollectionArt number="2" img="/collection2.svg" amount="150" />
+        <CollectionArt number="3" img="/collection3.svg" amount="75" />
+        <CollectionArt number="4" img="/collection4.svg" amount="75" />
+        <CollectionArt number="5" img="/collection5.svg" amount="33" />
+        <CollectionArt number="6" img="/collection6.svg" amount="24" />
+        <CollectionArt number="7" img="/collection7.svg" amount="22" />
+        <CollectionArt number="8" img="/collection8.svg" amount="64" />
+        <CollectionArt number="9" img="/collection9.svg" amount="877" />
+        <CollectionArt number="10" img="/collection10.svg" amount="55" />
+        <CollectionArt number="11" img="/collection11.svg" amount="35" />
+        <CollectionArt number="12" img="/collection12.svg" amount="32" />
+        <CollectionArt number="13" img="/collection13.svg" amount="94" />
+        <CollectionArt number="14" img="/collection14.svg" amount="75" />
+        <CollectionArt number="15" img="/collection15.svg" amount="25" />
+      </div>
     </div>
   );
 }
