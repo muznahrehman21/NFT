d95c6461cbfd34c8515f00641e3039b1ccc6d9d9
muznahrehman21 <mail2m.rehman@gmail.com>
2025-06-08 00:00:14 +0500

    minor fixes

--- src/components/SellerCollectionHeading.jsx
+++ src/components/SellerCollectionHeading.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 function SellerCollectionHeading() {
   return (
-    <div className="relative flex flex-col items-center h-[817px]">
+    <div className="flex flex-col items-center h-[817px]">
       <span className="font-apex font-bold text-[#FE0101] text-[96px]">
         JACOB JONES{" "}
       </span>
       <span className="font-apex font-bold text-[#2B2B2B] text-[64px]">
         COLLECTIONS
       </span>
-      <div className="w-[1320px] h-[633]">
-        <img src="/Group1.svg" alt="" className="bottom-30 absolute" />
+      <div className="relative w-[1320px] h-[633]">
+        <img src="/Group1.svg" alt="" className="-top-8 absolute" />
+        <img src="/Group2.svg" alt="" className="-top-20 right-0 absolute" />
+        <img src="/Group3.svg" alt="" className="-top-23 absolute" />
       </div>
     </div>
   );
