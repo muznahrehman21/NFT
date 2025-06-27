85354a352f13eeadbbc321b6e4e36678212c48e0
muznahrehman21 <mail2m.rehman@gmail.com>
2025-06-27 23:18:49 +0500

    component initiated

--- src/components/ArtistProfile/ProfileSwitch.jsx
+++ src/components/ArtistProfile/ProfileSwitch.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function ProfileSwitch() {
   return (
-    <div className="flex flex-row border-[#E7E7E7] border-[2px] rounded-[10px] w-[236px] h-[50px]"></div>
+    <div className="flex flex-row justify-center items-center gap-7 p-4 border-[#E7E7E7] border-[2px] rounded-[10px] w-[236px] h-[50px]">
+      <p className="font-roboto font-normal text-[#2B2B2B] text-[14px] cursor-pointer">
+        Profile
+      </p>
+      <p className="font-roboto font-normal text-[#2B2B2B] text-[14px] cursor-pointer">
+        Profile Setting
+      </p>
+    </div>
   );
 }
 
